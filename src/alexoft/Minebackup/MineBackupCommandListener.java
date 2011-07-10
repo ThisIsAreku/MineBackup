409370047231f56b8faa7732d3f43ca46b99e6d3
ThisIsAreku <this.is.areku@hotmail.fr>
2011-07-10 16:07:13 +0200

    Update to the latest CaftBukkit version

---
+++ src/alexoft/Minebackup/MineBackupCommandListener.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alexoft.Minebackup;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author Alexandre
+ */
+public class MineBackupCommandListener implements CommandExecutor {
+    private MineBackup plugin;
+    public MineBackupCommandListener(MineBackup plugin) { this.plugin = plugin; }
+    @Override
+    public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
+        if (!(cs instanceof Player)) {
+            return false;
+        }
+        Player player = (Player) cs;
+        if(!player.isOp()) return false;
+        this.plugin.executeSchedule(player.getName());
+        return true;
+    }
+    
+}
